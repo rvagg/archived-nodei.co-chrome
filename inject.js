6458b113beb4ff660c143f7f4e3b22d89322d078
Max Ogden <max@maxogden.com>
2014-01-08 10:34:55 -0800

    oops, change back to 6 months

--- inject.js
+++ inject.js
@@ -76,7 +76,7 @@ const $         = require('./ender')
         + '<img src="https://nodei.co/npm/{name}.png?downloads=true&stars=true&chrome" style="margin-right: 4px;">'
         + '</a>'
         + '<a href="https://nodei.co/npm/{name}/?chrome">'
-        + '<img src="https://nodei.co/npm-dl/{name}.png?months=1&chrome" style="margin-bottom: 6px;">'
+        + '<img src="https://nodei.co/npm-dl/{name}.png?months=6&chrome" style="margin-bottom: 6px;">'
         + '</a>'
         + '</div>'
 
