d2c31356048938fada440eeec1150d14e07ac0f3
Max Ogden <max@maxogden.com>
2014-01-08 10:32:05 -0800

    slightly smarter inserting

--- inject.js
+++ inject.js
@@ -71,21 +71,36 @@ module.exports = inject
 },{"./ender":2,"./insert_badge":5}],5:[function(require,module,exports){
 const $         = require('./ender')
     , badgeHtml =
-          '<div class="nodeico-badges" style="text-align: center;">'
-        + '<a href="https://nodei.co/npm/{name}/">'
-        + '<img src="https://nodei.co/npm/{name}.png?downloads=true&stars=true" style="margin-right: 4px;">'
+          '<div class="nodeico-badges">'
+        + '<a href="https://nodei.co/npm/{name}/?chrome">'
+        + '<img src="https://nodei.co/npm/{name}.png?downloads=true&stars=true&chrome" style="margin-right: 4px;">'
         + '</a>'
-        + '<a href="https://nodei.co/npm/{name}/">'
-        + '<img src="https://nodei.co/npm-dl/{name}.png?months=6" style="margin-bottom: 6px;">'
+        + '<a href="https://nodei.co/npm/{name}/?chrome">'
+        + '<img src="https://nodei.co/npm-dl/{name}.png?months=1&chrome" style="margin-bottom: 6px;">'
         + '</a>'
         + '</div>'
 
 function insert (name) {
-  var $fileList = $('.bubble.files-bubble')
-  if (!$fileList[0])
+  var existing = false
+  
+  $('img').map(function(img) {
+    if ($(img).attr('src').indexOf('nodei.co') > -1) existing = true
+  })
+  
+  if (existing) return
+  
+  var html = $(badgeHtml.replace(/\{name\}/g, name))
+  
+  var $readme = $('.entry-content')
+  if ($readme[0]) {
+    var $child = $readme.children(":first")
+    if ($child[0]) $($child[0]).after(html)
+    else $readme.prepend(html)
     return
-
-  $fileList.after($(badgeHtml.replace(/\{name\}/g, name)))
+  }
+  
+  var $fileList = $('.bubble.files-bubble')
+  if ($fileList[0]) return $fileList.after(html)
 }
 
 module.exports = insert
@@ -1685,9 +1700,9 @@ module.exports = insert
   }, true)
 }(ender));
 
-},{"bonzo":6,"qwery":"Av6yiO","qwery-mobile":"Av6yiO"}],"qwery":[function(require,module,exports){
-module.exports=require('Av6yiO');
-},{}],"Av6yiO":[function(require,module,exports){
+},{"bonzo":6,"qwery":"5S6tXD","qwery-mobile":"5S6tXD"}],"qwery":[function(require,module,exports){
+module.exports=require('5S6tXD');
+},{}],"5S6tXD":[function(require,module,exports){
 /*!
   * @preserve Qwery - A Blazing Fast query selector engine
   * https://github.com/ded/qwery
